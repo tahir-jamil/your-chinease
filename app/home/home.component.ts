a07a6a16db2a376f031218eac2031d494076d1d5
Zeeshan Aslam <zeeshan.sulehria@gmail.com>
2018-05-18 11:45:23 -0700

    first commit project working

---
+++ app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  moduleId: module.id
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  languageData = [
+    { english: "chair", chinese: "椅子" },
+    { english: "second", chinese: "椅子" },
+    { english: "third", chinese: "椅子" },
+    { english: "forth", chinese: "椅子" }
+];
+}
